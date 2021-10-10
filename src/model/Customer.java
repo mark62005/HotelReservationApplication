2a6845b1e7c2d3f89346ac5b76b0f6135507f321
Mark Wong <markwong.digital@gmail.com>
2021-10-10 15:29:13 -0700

    Add getters and setters for Customer's data fields

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -23,4 +23,28 @@ public class Customer {
 
     }
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
 }
