32047a84031c21313f04e327194226b2c30d3dbc
Mark Wong <markwong.digital@gmail.com>
2021-10-10 16:37:43 -0700

    Add addRoom method

--- src/service/ReservationService.java
+++ src/service/ReservationService.java
@@ -1,4 +1,25 @@
 package service;
 
+import model.IRoom;
+import model.Room;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class ReservationService {
+
+    private final Map<String, IRoom> rooms = new HashMap<>();
+
+    public void addRoom(IRoom room) {
+
+        String roomNumber = room.getRoomNumber();
+
+        // make sure all te room numbers in room map are unique
+        if (rooms.containsKey(roomNumber)) {
+            throw new IllegalArgumentException("Error! This room number is already taken, please try another one.");
+        }
+        rooms.put(roomNumber, room);
+
+    }
+
 }
