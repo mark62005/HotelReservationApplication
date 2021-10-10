5bf5e2054251be74475ab589816aedd6df1270c9
Mark Wong <markwong.digital@gmail.com>
2021-10-10 15:10:38 -0700

    Add getters and setters for Room data fields

--- src/model/Room.java
+++ src/model/Room.java
@@ -12,4 +12,28 @@ public class Room {
         this.roomType = roomType;
     }
 
+    public String getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(String roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Double getRoomPrice() {
+        return roomPrice;
+    }
+
+    public void setRoomPrice(Double roomPrice) {
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomType getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(RoomType roomType) {
+        this.roomType = roomType;
+    }
+
 }
