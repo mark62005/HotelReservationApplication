e03cb588a306dedd10519de433bf1bf2e0b4847e
Mark Wong <markwong.digital@gmail.com>
2021-10-10 15:37:39 -0700

    Add getters and setters Reservation's data fields

--- src/model/Reservation.java
+++ src/model/Reservation.java
@@ -16,4 +16,36 @@ public class Reservation {
         this.checkOutDate = checkOutDate;
     }
 
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
+    }
+
+    public IRoom getRoom() {
+        return room;
+    }
+
+    public void setRoom(IRoom room) {
+        this.room = room;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
 }
