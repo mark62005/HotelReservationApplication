6b79d93432252f930861819b334e2c6d8ec564c7
Mark Wong <markwong.digital@gmail.com>
2021-10-10 15:36:19 -0700

    Create Reservation Class

---
+++ src/model/Reservation.java
@@ -0,0 +1,12 @@
+package model;
+
+import java.util.Date;
+
+public class Reservation {
+
+    private Customer customer;
+    private IRoom room;
+    private Date checkInDate;
+    private Date checkOutDate;
+
+}
