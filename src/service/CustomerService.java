1c6171233e7a91a7ff4b57b39a798be120119497
Mark Wong <markwong.digital@gmail.com>
2021-10-10 16:07:13 -0700

    Add add customer method

--- src/service/CustomerService.java
+++ src/service/CustomerService.java
@@ -1,5 +1,23 @@
 package service;
 
+import model.Customer;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class CustomerService {
+
+    private Map<String, Customer> customers = new HashMap<>();
+
+    public void addCustomer(String firstName, String lastName, String email) {
+
+        Customer customer = new Customer(firstName, lastName, email);
+
+        if (customers.containsKey(email)) {
+            throw new IllegalArgumentException("Error! This email is taken, please try another one.");
+        }
+        customers.put(email, customer);
+
+    }
 
 }
