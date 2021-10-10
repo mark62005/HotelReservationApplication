a343401a7359665a791c9248d4f725543c5a3070
Mark Wong <markwong.digital@gmail.com>
2021-10-10 15:27:53 -0700

    Add an emailRegex to check the email format

--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.regex.Pattern;
+
 public class Customer {
 
     private String firstName;
@@ -7,9 +9,18 @@ public class Customer {
     private String email;
 
     public Customer(String firstName, String lastName, String email) {
+
+        String emailRegex = "^(.+)@(.+)\\.com$";
+        Pattern pattern = Pattern.compile(emailRegex);
+
+        // check if email format matches "domain@email.com"
+        if (!pattern.matcher(email).matches()) {
+            throw new IllegalArgumentException("Invalid format. Valid format for email: 'domain@email.com', please try again.");
+        }
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
+
     }
 
 }
