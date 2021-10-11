ca1f525a2c744e07cac6b6e78e7ec62f54518579
Mark Wong <markwong.digital@gmail.com>
2021-10-10 18:43:53 -0700

    Add printMainMenu method

--- src/menu/MainMenu.java
+++ src/menu/MainMenu.java
@@ -1,7 +1,29 @@
 package menu;
 
 public class MainMenu {
+
     public static void main(String[] args) {
 
+        // display initial menu
+        printMainMenu();
+
     }
+
+    public static void printMainMenu() {
+
+        String mainMenu = """
+                
+                Main Menu
+                1. Find and reserve a room
+                2. See my reservations
+                3. Create an account
+                4. Go to Admin Menu
+                5. Exit
+                Please enter your option (1 - 5):
+                """;
+
+        System.out.println(mainMenu);
+
+    }
+
 }
