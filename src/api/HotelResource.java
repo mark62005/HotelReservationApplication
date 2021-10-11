d15df2556efcf7a90179e24cc8956678b9201d3c
Mark Wong <markwong.digital@gmail.com>
2021-10-10 17:59:39 -0700

    Add static reference for HotelResource

--- src/api/HotelResource.java
+++ src/api/HotelResource.java
@@ -2,5 +2,16 @@ package api;
 
 public class HotelResource {
 
+    private static HotelResource instance;
+
+    // provide static reference
+    private HotelResource(){}
+
+    public static HotelResource getInstance() {
+        if (instance == null) {
+            instance = new HotelResource();
+        }
+        return instance;
+    }
 
 }
