c2cd4ffbeb8f22a971900f264009d78470f9a46f
Mark Wong <markwong.digital@gmail.com>
2021-10-11 01:46:26 -0700

    Add printAdminMenu method to AdminMenu

--- src/menu/AdminMenu.java
+++ src/menu/AdminMenu.java
@@ -1,7 +1,26 @@
 package menu;
 
 public class AdminMenu {
+
     public static void main(String[] args) {
 
+        // display initial admin menu
+        printAdminMenu();
+
+    }
+
+    public static void printAdminMenu() {
+        String adminMenu = """
+                
+                Admin Menu
+                1. See all Customers
+                2. See all Rooms
+                3. See all Reservations
+                4. Add a Room
+                5. Back to Main Menu
+                Please enter your option (1 - 5):
+                """;
+
+        System.out.println(adminMenu);
     }
 }
