6e1b5fe8bf7d851a9476132ed6e0d6d1de80569f
Mark Wong <markwong.digital@gmail.com>
2021-10-10 18:24:42 -0700

    Add static reference for AdminResource

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -2,4 +2,15 @@ package api;
 
 public class AdminResource {
 
+    private static AdminResource instance;
+
+    private AdminResource(){}
+
+    public static AdminResource getInstance() {
+        if (instance == null) {
+            instance = new AdminResource();
+        }
+        return instance;
+    }
+
 }
