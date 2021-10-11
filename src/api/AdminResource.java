ba8b6e571abd644425844e944a27d40bcea18c56
Mark Wong <markwong.digital@gmail.com>
2021-10-10 18:28:27 -0700

    Add addRoom method to AdminResource

--- src/api/AdminResource.java
+++ src/api/AdminResource.java
@@ -1,12 +1,15 @@
 package api;
 
 import model.Customer;
+import model.IRoom;
 import service.CustomerService;
+import service.ReservationService;
 
 public class AdminResource {
 
     private static AdminResource instance;
     private CustomerService customerService;
+    private ReservationService reservationService;
 
     // provide static reference
     private AdminResource(){}
@@ -20,6 +23,10 @@ public class AdminResource {
 
     public Customer getCustomer(String email) {
         return customerService.getCustomer(email);
+    }
+
+    public void addRoom(IRoom room) {
+        reservationService.addRoom(room);
     }
 
 }
